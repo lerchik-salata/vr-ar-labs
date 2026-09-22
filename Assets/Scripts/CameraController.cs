1ca23d08a0e76710fe7b14d3f83adf06b89dadf6
lerchik-salata <lerasalata@gmail.com>
2026-09-22 12:20:31 +0300

    Fix camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float lookSpeed = 2f;
 
     private float rotationX = 0f;
@@ -19,12 +18,6 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical   = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * horizontal + transform.forward * vertical;
-        transform.position += move * moveSpeed * Time.deltaTime;
-
         rotationX += Input.GetAxis("Mouse X") * lookSpeed;
         rotationY -= Input.GetAxis("Mouse Y") * lookSpeed;
         rotationY = Mathf.Clamp(rotationY, -90f, 90f);
